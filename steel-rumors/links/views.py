c5882c1848c0d473028fe918ff6b939b69d32e32
gzpgg3x <gzpgg3x@yahoo.com>
2014-12-25 20:08:39 -0500

    rerankAppearsToWork
    rerankAppearsToWork

--- steel-rumors/links/views.py
+++ steel-rumors/links/views.py
@@ -10,6 +10,13 @@ from django.core.urlresolvers import reverse_lazy
 from django.views.generic.edit import UpdateView
 from django.views.generic.edit import DeleteView
 from django.contrib.comments.models import Comment
+from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
+from django.views.generic.edit import FormView
+from .forms import VoteForm
+from .models import Vote
+import json
+from django.http import HttpResponse
 
 class RandomGossipMixin(object):
     def get_context_data(self, **kwargs):
@@ -21,6 +28,16 @@ class LinkListView(RandomGossipMixin, ListView):
     model = Link
     queryset = Link.with_votes.all()
     paginate_by = 20
+
+    def get_context_data(self, **kwargs):
+        context = super(LinkListView, self).get_context_data(**kwargs)
+        if self.request.user.is_authenticated():
+            voted = Vote.objects.filter(voter=self.request.user)
+            links_in_page = [link.id for link in context["object_list"]]
+            voted = voted.filter(link_id__in=links_in_page)
+            voted = voted.values_list('link_id', flat=True)
+            context["voted"] = voted
+        return context    
 
 class LinkDetailView(DetailView):
     model = Link   
@@ -65,4 +82,66 @@ class LinkUpdateView(UpdateView):
 
 class LinkDeleteView(DeleteView):
     model = Link
-    success_url = reverse_lazy("home")                   
+    success_url = reverse_lazy("home")   
+
+# class VoteFormView(FormView):
+#     form_class = VoteForm
+
+#     def form_valid(self, form):
+#         link = get_object_or_404(Link, pk=form.data["link"])
+#         user = self.request.user
+#         prev_votes = Vote.objects.filter(voter=user, link=link)
+#         has_voted = (prev_votes.count() > 0)
+
+#         if not has_voted:
+#             # add vote
+#             Vote.objects.create(voter=user, link=link)
+#             print("voted")
+#         else:
+#             # delete vote
+#             prev_votes[0].delete()
+#             print("unvoted")
+
+#         return redirect("home")
+
+#     def form_invalid(self, form):
+#         print("invalid")
+#         return redirect("home")                    
+
+class JSONFormMixin(object):
+    def create_response(self, vdict=dict(), valid_form=True):
+        response = HttpResponse(json.dumps(vdict), content_type='application/json')
+        response.status = 200 if valid_form else 500
+        return response
+
+class VoteFormBaseView(FormView):
+    form_class = VoteForm
+
+    def create_response(self, vdict=dict(), valid_form=True):
+        response = HttpResponse(json.dumps(vdict))
+        response.status = 200 if valid_form else 500
+        return response
+
+    def form_valid(self, form):
+        link = get_object_or_404(Link, pk=form.data["link"])
+        user = self.request.user
+        prev_votes = Vote.objects.filter(voter=user, link=link)
+        has_voted = (len(prev_votes) > 0)
+
+        ret = {"success": 1}
+        if not has_voted:
+            # add vote
+            v = Vote.objects.create(voter=user, link=link)
+            ret["voteobj"] = v.id
+        else:
+            # delete vote
+            prev_votes[0].delete()
+            ret["unvoted"] = 1
+        return self.create_response(ret, True)
+
+    def form_invalid(self, form):
+        ret = {"success": 0, "form_errors": form.errors }
+        return self.create_response(ret, False)
+
+class VoteFormView(JSONFormMixin, VoteFormBaseView):
+    pass
